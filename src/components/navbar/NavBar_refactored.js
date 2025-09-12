ad1a4474bd0718d029503e8923fee0256c96bc3d
RainKaixin <shuwang602@gmail.com>
2025-09-12 11:41:27 -0400

    Update NavBar_refactored.js

--- src/components/navbar/NavBar_refactored.js
+++ src/components/navbar/NavBar_refactored.js
@@ -40,8 +40,10 @@ const NavBar = () => {
     <nav className='fixed top-0 left-0 right-0 bg-white shadow-sm z-50'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between items-center h-16'>
-          {/* Logo */}
-          <NavbarLogo onClick={actions.handleLogoClick} />
+          {/* Logo - Hidden on mobile */}
+          <div className='hidden md:block'>
+            <NavbarLogo onClick={actions.handleLogoClick} />
+          </div>
 
           {/* Center Upload Button - Absolute Centering - Hidden on mobile */}
           <div className='absolute left-1/2 transform -translate-x-1/2 hidden md:block'>
